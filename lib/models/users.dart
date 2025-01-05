7418c984008354898e9b1a8dd3db641a2a92f14c
zekeriya2280 <zekeriyacoskun2280@gmail.com>
2025-01-05 16:14:13 +0900

    user model products changed for fb user products to fit

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -4,26 +4,31 @@ import 'package:zelix_kingdom/models/factory.dart';
 import 'package:zelix_kingdom/models/product.dart';
 
 class Users {
+  final String id;
   final String nickname;
   final String email;
   int money = 0;
-  List<Map<Product, int>>  products = [];
+  List<Map<String, Product>>  products = [];
   List<Factory> factories = [];
   List<City> cities = [];
-  Users({ required this.nickname, required this.email , required this.money, required this.factories, required this.products, required this.cities});
+  Users({ required this.id ,required this.nickname, required this.email , required this.money, required this.factories, required this.products, required this.cities});
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'nickname': nickname,
       'email': email,
       'money': money,
       'factories': factories.map((e) => e.toMap()).toList(),
-      'products': products.map((productMap) => productMap.map((product, amount) => MapEntry(product.toJson(), amount))).toList(),
+      'products.': products.map((e) => e).toList(),
       'cities': cities.map((e) => e.toJson()).toList(),
     };
   }
 
   factory Users.fromMap(Map<String, dynamic> material) {
+    if (material['id'] == null) {
+      throw ArgumentError('material must contain id');
+    }
     if (material['nickname'] == null) {
       throw ArgumentError('material must contain nickname');
     }
@@ -44,11 +49,12 @@ class Users {
     }
 
     return Users(
+      id: material['id'],
       nickname: material['nickname'],
       email: material['email'],
       money: material['money'],
       factories: List<Factory>.from(material['factories']),
-      products: material['products'].map((product, amount) => MapEntry(Product.fromJson(product), amount)),
+      products: material['products'].map((id, product) => MapEntry(Product.fromJson(product), id)).toList(),
       cities: List<City>.from(material['cities']),
     );
   }
